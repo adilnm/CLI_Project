37ad5f9babad21e91cd800effb9da949a418565e
ADIL NAIMI <adilnm23@hotmail.com>
2019-04-17 08:36:21 +0000

    In the CLIProject::Scraper class I start scraping the distrubution of every bird using the url that i scraped earlier

--- lib/CLI_Project/scraper.rb
+++ lib/CLI_Project/scraper.rb
@@ -10,10 +10,17 @@ class CLIProject::Scraper
       instan=CLIProject::Aviary.new
       instan.name=item.text
       instan.url=link+item.attr("href")
+      html1=open(instan.url)
+      doc1 = Nokogiri::HTML(html1)
+      instan.distribution=doc1.css("div.rightCol p").first.text
+    
     end
+  end
+  
+  def getData(url)
     
-    
-
   end
   
 end
+
+##<CLIProject::Aviary:0x0000000001c49838 @name="African Pygmy-goose", @url="https://www.aviary.org/animals/African-Pygmy-goose">
