274cc74902df3d224a45545daa58e6958074f375
ADIL NAIMI <adilnm23@hotmail.com>
2019-04-18 04:37:53 +0000

    built a getDistribution function inside the CLIProject::Aviary class to scrape distrubution for every bird instance

--- lib/CLI_Project/aviary.rb
+++ lib/CLI_Project/aviary.rb
@@ -2,12 +2,29 @@ class CLIProject::Aviary
   
   attr_accessor :name, :url, :description, :distribution, :habitat, :diet, :breeding, :status, :funFact
   @@all=[]
-  def initialize 
+  def initialize (name=nil, url=nil,description=nil,distribution=nil,habitat=nil,diet=nil,breeding=nil,status=nil,funFact=nil)
+    @name=name
+    @url=url
+    # @description=description
+    # @distribution=distribution
+    # @habitat=habitat
+    # @diet=diet
+    # @breeding=breeding
+    # @status=status
+    # @funFact=funFact
      @@all<< self
   end
   
   def self.all
     @@all
+  end
+  
+  def doc
+    Nokogiri::HTML(open(self.url))
+  end
+  
+  def getDistribution
+     @distribution=doc.css("div.rightCol p").first.text
   end
   
 end
--- lib/CLI_Project/scraper.rb
+++ lib/CLI_Project/scraper.rb
@@ -7,22 +7,29 @@ class CLIProject::Scraper
     doc = Nokogiri::HTML(html)
     
     doc.css("tr td.commonNameCell a").each do |item|
+      counter=1
       instan=CLIProject::Aviary.new
       instan.name=item.text
       instan.url=link+item.attr("href")
-      html1=open(instan.url)
-      doc1 = Nokogiri::HTML(html1)
-      instan.distribution=doc1.css("div.rightCol p").first.text
-      instan.habitat=doc1.css("div.rightCol p")[1].text
-      instan.diet=doc1.css("div.rightCol p")[2].text
-      instan.breeding=doc1.css("div.rightCol p")[3].text
-      instan.status=doc1.css("div.rightCol p")[4].text
-      instan.funFact=doc1.css("div.funFact").text.gsub("FUN FACT","").strip
-      instan.description=doc1.css("div.centerContent p").text
-      # binding.pry
+      # html1=open(instan.url)
+      # doc1 = Nokogiri::HTML(html1)
+      # instan.distribution=doc1.css("div.rightCol p").first.text
+      # instan.habitat=doc1.css("div.rightCol p")[1].text
+      # instan.diet=doc1.css("div.rightCol p")[2].text
+      # instan.breeding=doc1.css("div.rightCol p")[3].text
+      # instan.status=doc1.css("div.rightCol p")[4].text
+      # instan.funFact=doc1.css("div.funFact").text.gsub("FUN FACT","").strip
+      # instan.description=doc1.css("div.centerContent p").text
+      # # binding.pry
     end
   end
-  
+   
+  # def getDistribution(url)
+  #   instan=CLIProject::Aviary.new
+  #   html1=open(url)
+  #     doc1 = Nokogiri::HTML(html1)
+  #     instan.distribution=doc1.css("div.rightCol p").first.text
+  # end
   
   
 end
