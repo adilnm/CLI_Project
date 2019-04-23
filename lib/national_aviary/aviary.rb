6d6058c023b9bb87c180e56a3d9e759f73e65c45
Adil Naimi <adilnm23@gmail.com>
2019-04-23 15:55:17 -0500

    change the NationalAviary::Aviary doc method to prvent scraping from the website everytime it gets called

--- lib/national_aviary/aviary.rb
+++ lib/national_aviary/aviary.rb
@@ -1,5 +1,5 @@
 class NationalAviary::Aviary
-  
+
   attr_accessor :name, :url, :description, :distribution, :habitat, :diet, :breeding, :status, :funFact
   @@all = []
   def initialize (name = nil, url = nil,description = nil,distribution = nil,habitat = nil,diet = nil,breeding = nil,status = nil,funFact = nil)
@@ -7,44 +7,44 @@ class NationalAviary::Aviary
     @url = url
     @@all<< self
   end
-  
+
   def self.all
     @@all
   end
-  
+
   def doc
-    Nokogiri::HTML(open(self.url))
-  end
-  
-  def getDistribution
-     @distribution = doc.css("div.rightCol p").first.text
-  end
-  
-  def getDescription
-     @description = doc.css("div.centerContent p").text
-  end
-  
-  def getHabitat
-     @habitat = doc.css("div.rightCol p")[1].text
-  end
-  
-  def getDiet
-     @diet = doc.css("div.rightCol p")[2].text
-  end
-  
-  def getBreeding
-     @breeding = doc.css("div.rightCol p")[3].text
-  end
-  
-  def getStatus
-     @status = doc.css("div.rightCol p")[4].text
-  end
-  
-  def getFunFact
-     @funFact = doc.css("div.funFact").text.gsub("FUN FACT","").strip
+
+     @link ||= Nokogiri::HTML(open(self.url))
   end
 
- 
-  
+  def getDistribution
+     @distribution ||= doc.css("div.rightCol p").first.text
+  end
+
+  def getDescription
+     @description ||= doc.css("div.centerContent p").text
+  end
+
+  def getHabitat
+     @habitat ||= doc.css("div.rightCol p")[1].text
+  end
+
+  def getDiet
+     @diet ||= doc.css("div.rightCol p")[2].text
+  end
+
+  def getBreeding
+     @breeding ||= doc.css("div.rightCol p")[3].text
+  end
+
+  def getStatus
+     @status ||= doc.css("div.rightCol p")[4].text
+  end
+
+  def getFunFact
+     @funFact ||= doc.css("div.funFact").text.gsub("FUN FACT","").strip
+  end
+
+
+
 end
-  
